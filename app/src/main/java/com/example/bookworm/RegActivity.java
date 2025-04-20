3d93b0a7069f43e735c3d94f119b14049a16a312
AnyaV <ananvoitiuk06@gmail.com>
2025-04-20 23:02:34 +0300

    The connected firebase2.0 + registration is ready

--- app/src/main/java/com/example/bookworm/RegActivity.java
+++ app/src/main/java/com/example/bookworm/RegActivity.java
@@ -1,14 +1,28 @@
 package com.example.bookworm;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegActivity extends AppCompatActivity {
+
+    private EditText loginEditText, emailEditText, passwordEditText, guessPasswordEditText;
+    private Button regButton;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,59 @@ public class RegActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        loginEditText = findViewById(R.id.loginEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        guessPasswordEditText = findViewById(R.id.guessPasswordEditText);
+
+        regButton = findViewById(R.id.regButton);
+
+        regButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
+
+    }
+    private void registerUser() {
+        String login = loginEditText.getText().toString().trim();
+        String email = emailEditText.getText().toString().trim();
+        String password = passwordEditText.getText().toString().trim();
+
+        if(TextUtils.isEmpty(login)) {
+            emailEditText.setError("Введіть логін");
+            return;
+        }
+
+        if(TextUtils.isEmpty(email)) {
+            emailEditText.setError("Введіть email");
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            passwordEditText.setError("Введіть пароль");
+            return;
+        }
+
+        if(password.length() < 8) {
+            passwordEditText.setError("Пароль має бути не менше 8 символів");
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    if(task.isSuccessful()) {
+                        FirebaseUser user = mAuth.getCurrentUser();
+                        Toast.makeText(RegActivity.this, "Реєстрація успішна!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(RegActivity.this, "Помилка реєстрації: " + task.getException().getMessage(),
+                                Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 }
