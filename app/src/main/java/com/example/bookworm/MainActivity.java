65cfed135228d143c557345e1ef22bcc4a0674c2
AnyaV <ananvoitiuk06@gmail.com>
2025-04-21 20:21:52 +0300

    BottomNavigationBar

--- app/src/main/java/com/example/bookworm/MainActivity.java
+++ app/src/main/java/com/example/bookworm/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.bookworm;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +25,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        BottomNavigationView nav = findViewById(R.id.nav);
+        nav.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                // Перевіряємо, який пункт меню був вибраний
+                if (item.getItemId() == R.id.library) {
+                    return true;
+                } else if (item.getItemId() == R.id.home) {
+                    return true;
+                } else if (item.getItemId() == R.id.profile) {
+                    return true;
+                }
+                return false;
+            }
         });
     }
 }
--- app/src/main/java/com/example/bookworm/SignInActivity.java
+++ app/src/main/java/com/example/bookworm/SignInActivity.java
@@ -38,7 +38,7 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.signin_main);
+        setContentView(R.layout.activity_signin);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
